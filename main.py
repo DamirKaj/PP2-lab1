7845e5f066b3cb73113b2a967bfeeaa1937fd536
DamirKaj <remirearmost@gmail.com>
2025-09-19 04:13:16 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,135 @@
+"""
+print("Hello World")
+print(1+1)
+
+if 1==1:
+    print("Yes")
+
+x = 2
+x = "Damir"
+print(x)
+
+VAR="d"
+_VAR="d"
+Var="d"
+var="d"
+
+x, y, z = "Damir", "Dumb Hatef", "Dias"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Damir"
+print(x)
+print(y)
+print(z)
+
+kbtu = ["Damir", "Hatef", "Dias"]
+x, y, z = kbtu
+print(x)
+print(y)
+print(z)
+
+
+x = "I"
+y = "am"
+z = "smart"
+print(x, y, z)
+
+x = "awesome"
+
+def funkcia():
+  x = "Hitler"
+  print("Germani " + x)
+
+funkcia()
+
+print("Germani " + x)
+
+x = "Python rocks"                 # str
+x = 99                             # int
+x = -3.14                          # float
+x = 2+5j                           # complex
+x = ["Damir", "Hatef", "Roma"]        # list
+x = ("red", "green", "blue")       # tuple
+x = range(10)                      # range
+x = {"brand": "Tesla", "year": 2025}  # dict
+x = {"car", "bike", "train"}       # set
+x = frozenset({"gold", "silver", "bronze"})  # frozenset
+x = False                          # bool
+x = b"Data"                        # bytes
+x = bytearray(3)                   # bytearray
+x = memoryview(bytes(10))          # memoryview
+x = None                           # NoneType
+
+#x=int(5.2)=5
+#x=float(5)=5.0
+#x=str(5.23)=5.23 просто стр болып кетеді
+
+a = "Hello World!"
+print(a[6]) #will be "W"
+
+a = "Hello World!" #возвращает длину
+
+print(len(a))
+
+Damir = "I love you,Sh"
+print("love" in Damir) #будет true
+
+for x in "Damir":  #по порядку буквы выйдут
+  print(x)
+
+x = "I love you,Sh"
+if "love" in x:
+  print("Yes")
+
+x = "I love you,Sh"
+print("qpdq" not in x)
+
+x = "Hello World!"
+print(x[2:5]) #это типо как [2:5)
+
+x = "Hello World!"
+print(x[2:]) #llo World!
+
+x = "Hello World!"
+print(x[:5]) #Hello
+
+x = "Hello World!"
+print(x[-5:-2]) #orl
+
+x = "Hello World!"
+print(x.upper()) #HELLO WORLD!
+
+x = "Hello World!"
+print(x.lower()) #hello world
+
+x = "   hello world!   "
+print(x.strip())   # "hello world"
+
+x = "Hello World!"
+print(x.replace("H", "J")) #
+a = "Hello, World!"
+print(a.replace("H", "J"))
+a = "Hello, World!"
+print(a.replace("H", "J")) #Jello World!
+
+x="Hello World!"
+print(x.split(",")) #['Hello', ' World!'] запитая типо разделитель
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #HelloWorld
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c) #Hello World
+
+x = "I am \"Student\" "
+print(x)
+
+
+
+"""
